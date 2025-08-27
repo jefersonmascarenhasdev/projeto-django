0a9c0032de8a084d49fb1d99cab0efecb402be6b
Jeferson Mascarenhas <Jefersonmascarenhas49@gmail.com>
2025-08-26 22:20:14 -0300

    new commit

--- recipes/views.py
+++ recipes/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context={'name': 'Jeferson'
+                                                               })
 
